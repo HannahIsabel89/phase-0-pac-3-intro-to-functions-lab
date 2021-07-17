f0319ccf83fed30eb236476446453d196fdfb101
Hannah <hannahwoodward@hotmail.com>
2021-07-17 14:47:19 +0800

    Edited cide tested in replit

--- index.js
+++ index.js
@@ -2,19 +2,20 @@ function shout(string) {
     return string.toUpperCase();
   }
 function whisper(string) {
-    return string.toLowerCase();
-}
+    return string.toLowerCase();    
+    }
 function logShout() {
     console.log(shout("Hello));
 }
 function logWhisper() {
     console.log(whisper("Hello"));
-}
+    }
 function sayHiToGrandma(string){
     if(string === string.toLowerCase()){
         return ("I can/'t hear you!");
-    }else return (string === string.toUpperCase()) {
-        ("YES INDEED!");
+    }else if (string === string.toUpperCase()) {
+    return("YES INDEED!");
     }else if (string === "I love you, Grandma.") {
-        return ("I love you, too.");
+    return ("I love you, too.");
     }
+}
